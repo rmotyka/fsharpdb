5cb6b1666513d35aa70447598d4beb9ea7e9e448
Roman Motyka <r.motyka@cctechnology.pl>
2017-09-27 11:21:33 +0200

    better json serialization

--- UserService.fs
+++ UserService.fs
@@ -1,10 +1,11 @@
 module datab.UserService
 
 open Newtonsoft.Json
+open Newtonsoft.Json.Converters
 open Models
 
 let getUser (userRepoGetUser: string -> Async<(User seq)>) userName = 
     async {
         let! user = userRepoGetUser userName
-        return JsonConvert.SerializeObject(user)
-    }
+        return JsonConvert.SerializeObject(user, IdiomaticDuConverter())
+    }   
