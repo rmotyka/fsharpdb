11bda68e07df1e28d1be3d9ab1b1d5deef7114f6
Roman Motyka <r.motyka@cctechnology.pl>
2017-09-27 09:09:48 +0200

    And a test

--- Program.fs
+++ Program.fs
@@ -6,16 +6,17 @@ let main argv =
     let userList = UserService.getUser RepoUser.getUser "mxManager" |> Async.RunSynchronously
     userList |> printfn "%A"
 
-    // let testUser : Models.User = {
-    //     Id = 3;
-    //     Username = "xxx";
-    //     Password = "tttt";
-    //     Active = true;
-    //     Role = "r";
-    //     UserFacilityIdLista = Some "lll"
-    // }
+    let testUser : Models.User = {
+        Id = 3;
+        Username = "xxx";
+        Password = "tttt";
+        Active = true;
+        Role = "r";
+        UserFacilityIdLista = Some "lll"
+    }
 
-    // UserService.getUser (fun x -> [testUser]) "mxManager"
-    // |> printfn "%A"
+    UserService.getUser (fun x -> async { return Seq.ofList [testUser]}) "mxManager" 
+    |> Async.RunSynchronously
+    |> printfn "%A"
 
     0
