505e59cc500998ddc0cf8d76c78b0c6cf2d4dae1
Roman Motyka <r.motyka@cctechnology.pl>
2017-10-02 09:31:20 +0200

    Automatic reader to record conversion

--- RepoAdoBase.fs
+++ RepoAdoBase.fs
@@ -2,6 +2,7 @@ module datab.RepoAdoBase
 
 open Npgsql
 open System.Data.Common
+open Microsoft.FSharp.Reflection
 
 
 type UserX =
@@ -10,11 +11,24 @@ type UserX =
         Username: string
     }
 
-let ToUserX (reader: DbDataReader) =
-    { 
-        Id = unbox(reader.["Id"])
-        Username = unbox(reader.["Username"])
-    }
+let toRecordAut<'R> (reader: DbDataReader) = 
+    let rty = typeof<'R>
+    let allValues = FSharpType.GetRecordFields (rty)
+    let getValueFromReader name =
+        let ordinal = reader.GetOrdinal(name)
+        reader.GetValue(ordinal)
+    let vals = allValues |> Array.map (fun x -> getValueFromReader x.Name)
+
+    FSharpValue.MakeRecord(rty, vals, allowAccessToPrivateRepresentation = false) :?> 'R
+
+// let ToUserX (reader: DbDataReader) =
+//     { 
+//         Id = unbox(reader.["Id"])
+//         Username = unbox(reader.["Username"])
+//     }
+
+let ToUserX = toRecordAut<UserX>
+
 let dbConnection connectionString = 
     let conn = new NpgsqlConnection(connectionString)
     conn.Open()
